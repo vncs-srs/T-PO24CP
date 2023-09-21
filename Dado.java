818e92ed2ecdb4ddf4e5be1815cdabee5c174029
Angelo <angelojunior@alunos.utfpr.edu.br>
2023-09-21 10:10:18 -0300

    Dados

---
+++ Dado.java
@@ -0,0 +1,25 @@
+    import java.util.Random;
+public class Dado{
+  private int sideUp;
+
+   public Dado(){
+      Random x = new Random();
+      sideUp = x.nextInt(6) +1;
+   }
+   public Dado(int s){
+      sideUp=s;
+   }
+   public int getSideUp(){
+      return sideUp;
+   }
+   public void roll(){
+      Random x = new Random();
+      sideUp = x.nextInt(6) +1;
+   }
+   public String toString()
+   {
+        return "Lado do dado: "+sideUp ;
+        //arrumar isso aqui
+   }
+}
+
