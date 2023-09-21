2c17cf0d6fda72dd3726c230d3aeab5e7d20d971
Vinicius Soares <vsoares446@gmail.com>
2023-09-21 17:55:43 +0000

    Created

---
+++ Jogo-General/JogoGeneral.java
@@ -0,0 +1,14 @@
+public class JogoGeneral{
+    private int Dado[] dados;
+    private int jogadas;
+
+    public jogadas(int jogadas){
+        jogadas = 13;
+    }
+    public jogoGeneral(int dados, int jogadas){
+        
+    }
+    public void rolarDados(int dados){
+        
+    }
+}
