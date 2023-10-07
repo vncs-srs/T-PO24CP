2761d818e5e2da3fb5d6378dc4c8f63695b57038
Vinicius Soares <62343620+vncs-srs@users.noreply.github.com>
2023-10-07 18:26:28 -0300

    Update Menu

--- Jogo-General/usaCampeonato.java
+++ Jogo-General/usaCampeonato.java
@@ -1,19 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class usaCampeonato {
 
     public void menu() {
-        private Scanner scanner;
         char opcao;
+        String arquivo = "jogogeneralASCII.txt";
+        try (BufferedReader br = new BufferedReader(new FileReader(arquivo))) {
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                System.out.println(linha);
+            }
+        }
+
         do {
-            System.out.println("       _                      _____                           _ ");
-            System.out.println("      | |                    / ____|                         | |");
-            System.out.println("      | | ___   __ _  ___   | |  __  ___ _ __   ___ _ __ __ _| |");
-            System.out.println("  _   | |/ _ \ / _` |/ _ \  | | |_ |/ _ \ '_ \ / _ \ '__/ _` | |");
-            System.out.println(" | |__| | (_) | (_| | (_) | | |__| |  __/ | | |  __/ | | (_| | |");
-            System.out.println("  \____/ \___/ \__, |\___/   \_____|\___|_| |_|\___|_|  \__,_|_|");
-            System.out.println("                __/ |                                           ");
-            System.out.println("               |___/                                            ");
+            System.out.println("                > Menu: ");
             System.out.println("|--------------------------------------------------------------|");
             System.out.println("| a) Adicionar jogador                                         |");
             System.out.println("| r) Remover jogador                                           |");
@@ -22,7 +24,7 @@ public class usaCampeonato {
             System.out.println("| s) Sair                                                      |");
             System.out.println("|--------------------------------------------------------------|");
             System.out.print("Escolha uma opção: ");
-            opcao = scanner.next().charAt(0);
+            opcao = menu.next().charAt(0);
             
             switch (opcao) {
                 case 'a':
@@ -44,10 +46,12 @@ public class usaCampeonato {
                     System.out.println("Opção inválida. Tente novamente.");
                     break;
             }
+            System.out.flush();
         } while (opcao != 's');
     }
 
     public static void main(String[] args) {
+        Scanner menu = new Scanner (System.in );
         JogoGeneral jogo = new JogoGeneral();
         jogo.menu();
     }
