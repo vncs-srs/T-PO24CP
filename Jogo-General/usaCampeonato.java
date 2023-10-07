63fab275c2a96a74cf21232d0a44f0c67cc1d03d
Vinicius Soares <62343620+vncs-srs@users.noreply.github.com>
2023-10-07 12:57:26 -0300

    menu

--- Jogo-General/usaCampeonato.java
+++ Jogo-General/usaCampeonato.java
@@ -9,18 +9,18 @@ public class usaCampeonato {
             System.out.println("       _                      _____                           _ ");
             System.out.println("      | |                    / ____|                         | |");
             System.out.println("      | | ___   __ _  ___   | |  __  ___ _ __   ___ _ __ __ _| |");
-            System.out.println("  _   | |/ _ \\ / _` |/ _ \\  | | |_ |/ _ \\ '_ \\ / _ \\ '__/ _` | |");
+            System.out.println("  _   | |/ _ \ / _` |/ _ \  | | |_ |/ _ \ '_ \ / _ \ '__/ _` | |");
             System.out.println(" | |__| | (_) | (_| | (_) | | |__| |  __/ | | |  __/ | | (_| | |");
-            System.out.println("  \\____/ \\___/ \\__, |\\___/   \\_____|\\___|_| |_|\\___|_|  \\__,_|_|");
+            System.out.println("  \____/ \___/ \__, |\___/   \_____|\___|_| |_|\___|_|  \__,_|_|");
             System.out.println("                __/ |                                           ");
             System.out.println("               |___/                                            ");
-            System.out.println("|------------------------------------------------|");
-            System.out.println("| a) Adicionar jogador                           |");
-            System.out.println("| r) Remover jogador                             |");
-            System.out.println("| j) Jogar rodada                                |");
-            System.out.println("| m) Mostrar cartela                             |");
-            System.out.println("| s) Sair                                        |");
-            System.out.println("|------------------------------------------------|");
+            System.out.println("|--------------------------------------------------------------|");
+            System.out.println("| a) Adicionar jogador                                         |");
+            System.out.println("| r) Remover jogador                                           |");
+            System.out.println("| j) Jogar rodada                                              |");
+            System.out.println("| m) Mostrar cartela                                           |");
+            System.out.println("| s) Sair                                                      |");
+            System.out.println("|--------------------------------------------------------------|");
             System.out.print("Escolha uma opção: ");
             opcao = scanner.next().charAt(0);
             
