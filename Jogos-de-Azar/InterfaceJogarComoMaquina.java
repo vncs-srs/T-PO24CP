c7564316f8a063851fd37ef6560c01b0f7beb85a
Vinicius Soares <vsoares446@gmail.com>
2023-11-23 13:03:17 +0000

    Implementacao da interface da Maquina

--- Jogos-de-Azar/InterfaceJogarComoMaquina.java
+++ Jogos-de-Azar/InterfaceJogarComoMaquina.java
@@ -1,23 +1,5 @@
-public class InterfaceJogarComoMaquina{
-    public int aplicarEstrategia(int n){// n e o numero maximo de jogadas
-        int min = 1;
-        int max = n;// parametriar
-        int quantidadeNumeros = max - min + 1;
-        
-        boolean[] numerosGerados = new boolean[quantidadeNumeros];
-        Random random = new Random();
-        
-        int numerosRestantes = quantidadeNumeros;
-
-        while (numerosRestantes > 0) {
-            int numeroAleatorio = random.nextInt(quantidadeNumeros);
-
-            if (!numerosGerados[numeroAleatorio]) {
-                numerosGerados[numeroAleatorio] = true;
-                numerosRestantes--;
-                return numeroAleatorio+1;
-            }
-        }
-        return 0;
-    }
+public interface InterfaceJogarComoMaquina{
+    void escolherJogo();
+    void escolherJogada(int opcao);   
+    int Estrategia(int n);
 }
